5a0a660b167fe39936b795479f0093cee9045876
Preeternal <preeternal@ya.ru>
2022-01-07 13:32:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.orangeAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SomeText'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Text(
+          '123',
+          style: TextStyle(
+              fontSize: 20, color: Colors.redAccent, fontFamily: 'Cera Pro'),
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('pressed');
+          },
+          child: Text(
+            'press',
+          ),
+          backgroundColor: Colors.orangeAccent,
+        ),
+      ),
+    );
+  }
+}
